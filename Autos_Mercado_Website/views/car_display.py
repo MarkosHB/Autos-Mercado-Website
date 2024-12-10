f1fa051ef42fbb93deb1a2e0977d2745e0ee8e0d
MarkosHB <marcoshidalgobanos@gmail.com>
2024-12-10 17:28:35 +0100

    Fix botones detalles coche

--- Autos_Mercado_Website/views/car_display.py
+++ Autos_Mercado_Website/views/car_display.py
@@ -17,12 +17,13 @@ def car_display() -> rx.Component:
         rx.skeleton(rx.text("Prueba")),
 
         # Una vez cargado, mostrar los detalles del vehiculo.
-
         rx.vstack(
             dark_mode_toggle(),
             rx.blockquote(PageState.vehiculo_info.modelo, weight="bold", size="8", color_scheme="blue"),
             rx.container(
+
                 rx.desktop_only(
+
                     rx.hstack(
                         rx.image(
                             src=PageState.image,
@@ -30,10 +31,10 @@ def car_display() -> rx.Component:
                             border="1px solid gray",
                             width="60%"
                         ),
+
                         rx.vstack(
                             rx.heading(
-                                f"{PageState.vehiculo_info.precio_venta} €",
-                                weight="bold", size="9",
+                                f"{PageState.vehiculo_info.precio_venta} €", weight="bold", size="9",
                             ),
                             rx.heading(
                                 f"Financiado por {PageState.vehiculo_info.precio_financiado} €/mes",
@@ -55,35 +56,13 @@ def car_display() -> rx.Component:
                         width="100%",
                         gap=Size.VERY_BIG.value,
                     ),
-                    rx.hstack(
-                        rx.button(
-                            rx.link(
-                                title("Coches.net", size=[Size.DEFAULT_BIG.value]),
-                                href=PageState.vehiculo_info.coches_punto_net
-                            ),
-                            radius="large",
-                            size="4",
-                            color_scheme="red",
-                            variant="classic"
-                        ) if PageState.vehiculo_info.coches_punto_net is not None else rx.fragment(),
-                        rx.button(
-                            rx.link(
-                                title("Milanuncios", size=[Size.DEFAULT_BIG.value]),
-                                href=PageState.vehiculo_info.milanuncios
-                            ),
-                            radius="large",
-                            size="4",
-                            color_scheme="jade",
-                            variant="classic"
-                        ) if PageState.vehiculo_info.milanuncios is not None else rx.fragment(),
-                        gap=Size.DEFAULT.value,
-                        align="center",
-                        margin="20px"
-                    ),
+
+                    rx.box(botones_cochesnet_milanuncios(), margin="20px"),
 
                 ),
 
                 rx.mobile_and_tablet(
+
                     rx.vstack(
                         rx.image(
                             src=PageState.image,
@@ -91,6 +70,7 @@ def car_display() -> rx.Component:
                             border="1px solid gray",
                             width="100%"
                         ),
+
                         rx.vstack(
                             rx.heading(f"{PageState.vehiculo_info.precio_venta} €", weight="bold", size="9"),
                             rx.heading(
@@ -98,6 +78,7 @@ def car_display() -> rx.Component:
                                 weight="medium", size="5",
                             ),
                         ),
+
                         data_list(
                             v1=PageState.vehiculo_info.anio,
                             v2=PageState.vehiculo_info.tipo,
@@ -109,30 +90,9 @@ def car_display() -> rx.Component:
                             v8=PageState.vehiculo_info.puertas,
                             v9=PageState.vehiculo_info.color,
                         ),
-                        rx.vstack(
-                            rx.button(
-                                rx.link(
-                                    title("Coches.net", size=[Size.DEFAULT_BIG.value]),
-                                    href=PageState.vehiculo_info.coches_punto_net
-                                ),
-                                radius="large",
-                                size="4",
-                                color_scheme="red",
-                                variant="classic"
-                            ) if PageState.vehiculo_info.coches_punto_net is not "N/A" else rx.fragment(),
-                            rx.button(
-                                rx.link(
-                                    title("Milanuncios", size=[Size.DEFAULT_BIG.value]),
-                                    href=PageState.vehiculo_info.milanuncios
-                                ),
-                                radius="large",
-                                size="4",
-                                color_scheme="jade",
-                                variant="classic"
-                            ) if PageState.vehiculo_info.milanuncios is not "N/A" else rx.fragment(),
-                            gap=Size.DEFAULT.value,
-                            align="center",
-                        ),
+
+                        botones_cochesnet_milanuncios(),
+
                         align="center",
                         width="100%",
                         gap=Size.VERY_BIG.value,
@@ -146,4 +106,40 @@ def car_display() -> rx.Component:
             spacing=Spacing.BIG.value,
             padding=Size.DEFAULT.value,
         ),
+    )
+
+
+def botones_cochesnet_milanuncios() -> rx.Component:
+    return rx.flex(
+        rx.cond(
+            PageState.vehiculo_info.coches_punto_net == "N/A",
+            rx.fragment(),
+            rx.button(
+                rx.link(
+                    title("Coches.net", size=[Size.DEFAULT_BIG.value]),
+                    href=PageState.vehiculo_info.coches_punto_net
+                ),
+                radius="large",
+                size="4",
+                color_scheme="red",
+                variant="classic"
+            ),
+        ),
+        rx.cond(
+            PageState.vehiculo_info.milanuncios == "N/A",
+            rx.fragment(),
+            rx.button(
+                rx.link(
+                    title("Milanuncios", size=[Size.DEFAULT_BIG.value]),
+                    href=PageState.vehiculo_info.milanuncios
+                ),
+                radius="large",
+                size="4",
+                color_scheme="jade",
+                variant="classic"
+            )
+        ),
+        gap=Size.DEFAULT.value,
+        align="center",
+        wrap="wrap",
     )
