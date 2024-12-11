b1887a3577407542609009aa297556410f1ede28
MarkosHB <marcoshidalgobanos@gmail.com>
2024-12-11 19:30:16 +0100

    Vercel deploy (please)

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Autos_Mercado_Website",
-    api_url="https://autos-mercado-website.vercel.app"
+    api_url="https://autos-mercado-website.vercel.app:8000"
 )
