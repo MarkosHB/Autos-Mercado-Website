dca9770b096c2cfc44793ecc1cc991dd8994b8ce
MarkosHB <marcoshidalgobanos@gmail.com>
2024-12-12 14:04:13 +0100

    Public folder update Railway

--- rxconfig.py
+++ rxconfig.py
@@ -2,10 +2,10 @@ import reflex as rx
 
 config = rx.Config(
     app_name="autosmercado",
+    api_url="https://autos-mercado-website-production.up.railway.app",
 
 )
 
-#     api_url="autos-mercado-website-production.up.railway.app",
 #     cors_allowed_origins=[
 #         "http://localhost:3000"
 #         "https://autos-mercado-website.vercel.app"
