a5a8f3cc9bb01fe8d52daa985f3c4491b16acaf0
MarkosHB <marcoshidalgobanos@gmail.com>
2024-12-16 14:23:33 +0100

    fix

--- rxconfig.py
+++ rxconfig.py
@@ -3,8 +3,8 @@ import reflex as rx
 config = rx.Config(
     app_name="autosmercado",
     api_url="https://autos-mercado-website-production.up.railway.app",
-    cors_allowed_origins=[
-         "http://localhost"
-         "https://autos-mercado-website.vercel.app"
-    ]
+    #cors_allowed_origins=[
+    #     "http://localhost"
+    #     "https://autos-mercado-website.vercel.app"
+    #]
 )
