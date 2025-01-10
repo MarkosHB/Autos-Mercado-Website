01975c9595490eb5a13897cfddc8fbec783fb1d4
MarkosHB <marcoshidalgobanos@gmail.com>
2025-01-10 17:04:25 +0100

    Railway api_url update

--- rxconfig.py
+++ rxconfig.py
@@ -3,10 +3,11 @@ import os
 import dotenv
 
 dotenv.load_dotenv()
+RAILWAY_API_URL = os.getenv('RAILWAY_API_URL')
 
 config = rx.Config(
     app_name="autosmercado",
-    api_url=os.getenv('RAILWAY_API_URL'),
+    api_url=RAILWAY_API_URL,
     cors_allowed_origins=[
          "http://localhost:3000",
          "https://autos-mercado-website.vercel.app"
